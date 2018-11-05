cd2833f4a7d81c1dfe86567cc5e920498b0bd684
duyanpeng <215028726@qq.com>
2018-11-05 16:33:42 +0800

    初始化仓库

---
+++ layouts/default.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          v-for="(item, i) in items"
+          :to="item.to"
+          :key="i"
+          router
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon" />
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title" />
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>面试</v-btn>  
+      <v-btn flat>vue</v-btn>
+      <v-btn flat>react</v-btn>
+      <v-btn flat>node</v-btn>
+      <v-btn flat>移动端</v-btn>
+    </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 杜延鹏的前端博客</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: false,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
